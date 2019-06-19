15aa5cad2c1e24464e3fdf07d39464eb2febc16e
raberin <rmvaberin@gmail.com>
2019-06-18 20:18:56 -0700

    Added jest and initialized yarn

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("the enhancer", () => {
+  describe("the repair function", () => {
+    it("should restore the durability", () => {
+      //arrange
+      const longsword = {};
+
+      //act
+      const result = enhancer.repair(longsword);
+
+      //assert
+    });
+  });
+});
