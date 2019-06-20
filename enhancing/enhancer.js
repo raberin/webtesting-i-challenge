252b6beb13e2c87daf29c3628126c212577f04a2
raberin <rmvaberin@gmail.com>
2019-06-19 20:15:53 -0700

    All tests working MVP Complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,28 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement++;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement > 15) {
+    item.durability -= 10;
+    item.enhancement--;
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -5,12 +5,63 @@ describe("the enhancer", () => {
   describe("the repair function", () => {
     it("should restore the durability", () => {
       //arrange
-      const longsword = {};
+      const longsword = {
+        name: "longsword",
+        durability: 85,
+        enhancement: 5
+      };
 
       //act
       const result = enhancer.repair(longsword);
 
       //assert
+      expect(result.durability).toBe(100);
+    });
+  });
+  describe("the success function", () => {
+    it("should increase the enhancement value by 1", () => {
+      const longsword = {
+        name: "longsword",
+        durability: 85,
+        enhancement: 5
+      };
+
+      const result = enhancer.succeed(longsword);
+
+      expect(result.enhancement).toBe(6);
+    });
+  });
+
+  describe("the fail function", () => {
+    it("should decrease durability", () => {
+      const longsword = {
+        name: "longsword",
+        durability: 85,
+        enhancement: 5
+      };
+
+      const longsword1 = {
+        name: "longsword",
+        durability: 85,
+        enhancement: 17
+      };
+
+      const result = enhancer.fail(longsword);
+      const result1 = enhancer.fail(longsword1);
+
+      expect(result.durability).toBe(80);
+      expect(result1.durability).toBe(75);
+    });
+    it("should decrease enhancement", () => {
+      const longsword1 = {
+        name: "longsword",
+        durability: 85,
+        enhancement: 17
+      };
+
+      const result1 = enhancer.fail(longsword1);
+
+      expect(result1.enhancement).toBe(16);
     });
   });
 });
